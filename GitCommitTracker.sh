acbf4f56417e337c0d8f37db6328c4167474bb69
kassandrasmith <kassandrasmith@utexas.edu>
2016-04-28 23:41:34 -0500

    Fix comparison method

--- GitCommitTracker.sh
+++ GitCommitTracker.sh
@@ -2,11 +2,14 @@
 
 DATE= date +%Y-%m-%d
 echo $DATE
-git log --pretty=format:"%cd" --date=short > log.txt
 
+git log --pretty=format:"%cd" --date=short > log.txt
 COMMIT_DATE= head -n 1 log.txt
 echo $COMMIT_DATE
 
- if [ $DATE=$COMMIT_DATE ];
-	then echo "committed today"
- fi
+ if [[ "$DATE" -gt "$COMMIT_DATE" ]];
+then 	
+	echo "committed today";
+else 
+	echo "Need to commit!!!";
+ fi;
