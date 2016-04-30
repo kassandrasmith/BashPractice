30159835a79f7d18b9fb62748a9c82156e9061f1
kassandrasmith <kassandrasmith@utexas.edu>
2016-04-29 23:55:51 -0500

    Simplify retrieving date from commit log Add second comparison with unix seconds

--- GitCommitTracker.sh
+++ GitCommitTracker.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
 DATE= date +%Y-%m-%d
-echo $DATE
+#echo $DATE
 
-git log --pretty=format:"%cd" --date=short > log.txt
-COMMIT_DATE= head -n 1 log.txt
-echo $COMMIT_DATE
+LOG_DATE=$(git log -1 HEAD --pretty=format:"%cd" --date=short)
+#echo $LOG_DATE
 
-if [[ "$DATE" -eq "$COMMIT_DATE" ]];
+
+if [[ "$LOG_DATE" -eq "$DATE" ]];
 then
 	echo "committed today";
 else
 	echo "Need to commit!!!";
  fi;
+
+StartDate=$(date -u -d "$DATE" +"%s")
+FinalDate=$(date -u -d "$LOG_DATE" +"%s")
+#echo $FinalDate
+#echo $StartDate 
+
+
+if [[ "$StartDate" -eq "$FinalDate" ]];
+then
+        echo "committed today";
+else
+        echo "Need to commit!!!";
+ fi;
+
