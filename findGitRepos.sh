c43bc669941a675c756908e26c73667f8b91df13
kassandrasmith <kassandrasmith@utexas.edu>
2016-05-06 15:12:41 -0500

    Add line for getting first filepath

--- findGitRepos.sh
+++ findGitRepos.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-find ~ -type d -name .git | xargs -n 1 dirname > foundrepos.txt
+find ~ -type d -name .git | xargs -n 1 dirname > foundrepos.txt;
+line=$(head -1 file);
+cd $line;
+echo $line;
