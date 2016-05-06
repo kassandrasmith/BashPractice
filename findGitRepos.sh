b41b65f5684b5e548057083dbe28995000677636
kassandrasmith <kassandrasmith@utexas.edu>
2016-05-06 15:13:37 -0500

    Revert "Add line for getting first filepath"
    This reverts commit c43bc669941a675c756908e26c73667f8b91df13.

--- findGitRepos.sh
+++ findGitRepos.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-find ~ -type d -name .git | xargs -n 1 dirname > foundrepos.txt;
-line=$(head -1 file);
-cd $line;
-echo $line;
+find ~ -type d -name .git | xargs -n 1 dirname > foundrepos.txt
